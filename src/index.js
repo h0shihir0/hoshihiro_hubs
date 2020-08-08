2bd79c7708b8612dcb6e038fd420615e7c5e2de0
星野寛人 <hoshiro1998@icloud.com>
2020-08-08 15:44:14 +0900

    000000

--- src/index.js
+++ src/index.js
@@ -9,15 +9,8 @@ import { lang, messages } from "./utils/i18n";
 import "./assets/stylesheets/globals.scss";
 import { AuthContextProvider } from "./react-components/auth/AuthContext";
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+app.set('port', (process.env.PORT || 5000));
 
 registerTelemetry("/home", "Hubs Home Page");
 
