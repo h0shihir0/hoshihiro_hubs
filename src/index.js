521ec152fbb9974e09d1f0b560b1c6f81fee3e9b
星野寛人 <hoshiro1998@icloud.com>
2020-08-08 14:43:13 +0900

    KOKOKO

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import { HomePage } from "./react-components/home/HomePage";
 import { lang, messages } from "./utils/i18n";
 import "./assets/stylesheets/globals.scss";
 import { AuthContextProvider } from "./react-components/auth/AuthContext";
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 registerTelemetry("/home", "Hubs Home Page");
 
